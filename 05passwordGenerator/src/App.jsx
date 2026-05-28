85fa9aab1354efcfb34c0b1613a95759204afc71
Mahima <mahimaanchra@gmail.com>
2026-05-28 23:00:39 +0530

    useCallback useEffect

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState , useCallback } from 'react'
+import { useState , useCallback ,useEffect } from 'react'
 
 function App() {
   const [length, setLength] = useState(8)
@@ -14,17 +14,18 @@ function App() {
 
     for(let i = 1 ; i <= length ; i++){
       let char = Math.floor(Math.random()*str.length + 1);
-      pass = str.charAt(char);
-      setPassword(pass)
+      pass = pass + str.charAt(char);
     }
+     setPassword(pass);
   } , [length,numAllowed,charAllowed,setPassword])
+ 
+  useEffect(()=>{passwordGenerator()} , [length , numAllowed , charAllowed , passwordGenerator])
 
   return (
     <>
      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-800 p-4">
       <h1 className='text-white text-center my-3 text-2xl font-semibold'>Password generator</h1>
       
-      {/* 🛠️ CHANGER CONTAINER: Combined them cleanly into one white bar */}
       <div className="flex shadow items-center overflow-hidden mb-4 bg-white rounded-xl">
         <input 
           type="text" 
