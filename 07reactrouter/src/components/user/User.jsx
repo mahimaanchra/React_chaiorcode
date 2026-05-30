af8c5064dddcd811c936112130940d2caed72d33
Mahima <mahimaanchra@gmail.com>
2026-05-31 01:16:05 +0530

    completed 07

--- 07reactrouter/src/components/user/User.jsx
+++ 07reactrouter/src/components/user/User.jsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 function User(){
     const {userid} = useParams();
     return(
-        <div className="flex justify-center">User:{userid}</div>
+        <div className="flex justify-center bg-gray-700 h-6 text-white">User : {userid}</div>
     )
 }
 
--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -8,6 +8,7 @@ import About from './components/about/About.jsx'
 import Layout from './Layout.jsx'
 import Contact from './components/contact/contact.jsx'
 import User from './components/user/User.jsx'
+import Github, { githubInfoLoader } from './components/github/Github.jsx'
 // const router = createBrowserRouter([
 //   {
 //      path : '/',
@@ -36,6 +37,7 @@ const router = createBrowserRouter(
 <Route path='about' element={<About/>}/>
 <Route path='contact' element={<Contact/>}/>
 <Route path='user/:userid' element={<User/>}/>
+<Route loader = {githubInfoLoader} path='github' element={<Github/>}/>
     </Route>
   )
 )
